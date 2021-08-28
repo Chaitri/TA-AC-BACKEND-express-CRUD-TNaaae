ba015bbf109f18569cd3ac4c0e18b049fcf646cc
Chaitri <chaitri.s.mohan@gmail.com>
2021-08-28 12:41:56 +0530

    Complete Assignment II

---
+++ block-BNaadi/user-diary-2/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, lowercase: true },
+    age: Number,
+    bio: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
